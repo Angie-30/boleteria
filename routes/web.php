2949de6cecf161b47c117c1e47b80e0903a96286
Angie-30 <ximesanabria30@gmail.com>
2025-10-21 19:40:46 -0500

    Primeros pasos en ventana de registro eventos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/* Route::get('/evento', function () {
+    return view('evento');
+}); */
+
+Route::get('/evento', [EventoController::class, 'create']);
+
+Route::get('/alerta', function () {
+    return view('alerta');
 });
